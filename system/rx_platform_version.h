5af0d4221f3a9c735994b59e60fd604f7ac313de
cira49152 <cira49152@gmail.com>
2017-12-03 02:08:23 +0100

    Added soft logic code, handled console cancel interactive and telnet
    System Information
    =============================================
    Engine Version: Atom Ver 0.5.7.20171202
    Library Version:  Ver 0.9.5.20171201
    OS/HW Interface: Win32 Ver 0.10.1.20171128 ; GNU Ver 0.10.1.20171202

--- system/rx_platform_version.h
+++ system/rx_platform_version.h
@@ -2,7 +2,7 @@
 
 #include "version/rx_version.h"
 
-#define RX_SERVER_NAME "Atom"
+#define RX_SERVER_NAME "Atom" 
 #define RX_SERVER_MAJOR_VERSION 0
 #define RX_SERVER_MINOR_VERSION 5
 #define RX_SERVER_BUILD_NUMBER 7
