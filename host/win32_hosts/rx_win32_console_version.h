74ac86fb3279da4c716ac5db2e02dcea87d87692
Dusan Ciric <cira49152@gmail.com>
2024-02-05 13:21:51 +0100

    aaaa

--- host/win32_hosts/rx_win32_console_version.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "version/rx_version.h"
-
-#define RX_WIN32_HOST_NAME "Win32 Console"
-#define RX_WIN32_HOST_MAJOR_VERSION 0
-#define RX_WIN32_HOST_MINOR_VERSION 6
-#define RX_WIN32_HOST_BUILD_NUMBER 0
-
